bd8e4d5e8d0d91450a7edb9b530be64a80570fe0
Osnago-L <osna.brera@gmail.com>
2022-01-11 17:48:41 +0100

    end project

--- functions.js
+++ functions.js
@@ -4,7 +4,7 @@ function createPosts(i) {
             <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="${posts[i].media}" alt="${posts[i].author.name}">                    
+                        <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
                     </div>
                     <div class="post-meta__data">
                         <div class="post-meta__author">${posts[i].author.name}</div>
@@ -14,18 +14,18 @@ function createPosts(i) {
             </div>
             <div class="post__text">${posts[i].content}</div>
             <div class="post__image">
-                <img src="${posts[i].author.image}" alt="">
+                <img src="${posts[i].media}" alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button js-like-button" href="#" data-postid="1">
+                        <a class="like-button js-like-button">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                        Piace a <b class="js-likes-counter">${posts[i].likes}</b> persone
                     </div>
                 </div> 
             </div>            
--- main.js
+++ main.js
@@ -7,10 +7,10 @@ const posts = [
     {
         "id": 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/300/300?image=171",
+        "media": "https://unsplash.it/600/300?image=15",
         "author": {
             "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
+            "image": "https://unsplash.it/300/300?image=171"
         },
         "likes": 80,
         "created": date
@@ -19,10 +19,10 @@ const posts = [
     {
         "id": 2,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/300/300?image=112",
+        "media": "https://unsplash.it/600/300?image=10",
         "author": {
             "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
+            "image": "https://unsplash.it/300/300?image=112"
         },
         "likes": 120,
         "created": date
@@ -30,10 +30,10 @@ const posts = [
     {
         "id": 3,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/300/300?image=234",
+        "media": "https://unsplash.it/600/300?image=20",
         "author": {
             "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
+            "image": "https://unsplash.it/300/300?image=234"
         },
         "likes": 78,
         "created": date
@@ -41,10 +41,10 @@ const posts = [
     {
         "id": 4,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "",
+        "media": "https://unsplash.it/600/300?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": "https://unsplash.it/300/300?image=24"
+            "image": null
         },
         "likes": 56,
         "created": date
@@ -52,15 +52,17 @@ const posts = [
     {
         "id": 5,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/300/300?image=534",
+        "media": "https://unsplash.it/600/300?image=29",
         "author": {
             "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
+            "image": "https://unsplash.it/300/300?image=534"
         },
         "likes": 95,
         "created": date
     }
 ];
+
+
 
 for (let i = 0; i < posts.length; i++) {
     createPosts(i)
@@ -69,17 +71,31 @@ for (let i = 0; i < posts.length; i++) {
 const likesbuttons = document.querySelectorAll(".like-button");
 const likesTot = document.querySelectorAll(".js-likes-counter");
 const dateFormat = document.querySelectorAll(".post-meta__time");
+const noProfileDefault = document.querySelectorAll(".post-meta__icon");
+
 for (let i = 0; i < posts.length; i++) {
+    if(posts[i].author.image == null){
+        let defaultName = posts[i].author.name.split(" ");
+        noProfileDefault[i].classList.add("profile-pic-default");
+        noProfileDefault[i].innerHTML=`<span>${defaultName[0].charAt(0)}${defaultName[1].charAt(0)}</span>`;
+        
+    };
+}
+
+ for (let i = 0; i < posts.length; i++) {
     likesbuttons[i].addEventListener("click",function(){
+
         if(this.classList.contains("like-button--liked")){
+
             this.classList.remove("like-button--liked");
             likesTot[i].innerHTML=likeDecrement(i);
             dateFormat[i].innerHTML= `${mm}/${dd}/${yyyy}`;
 
         }else{
+
             this.classList.add("like-button--liked");
             likesTot[i].innerHTML=likeIncrement(i);
             dateFormat[i].innerHTML= `${dd}/${mm}/${yyyy}`;
         }
-    });
-}
+    }); 
+ }
